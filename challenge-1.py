4f7856d0a674f46b246b570b27cb75792ae651e5
andacuzel <andacuz@gmail.com>
2025-05-12 20:04:18 +0300

    Gün 3: pozitif/negatif ve çift tek görevleri eklendi

---
+++ challenge-1.py
@@ -0,0 +1,6 @@
+sayi = int(input("Bir sayı girin: "))
+
+if sayi %2 == 0:
+    print("Girdiğiniz değer bir çift sayıdır.")
+else:
+    print("Girdiğiniz değer bir tek sayıdır.")
---
+++ gorev-4.py
@@ -0,0 +1,8 @@
+sayi = int(input("Bir sayı girin: "))
+
+if sayi < 0:
+    print("Negatif bir sayı girdiniz")
+elif sayi == 0:
+    print("Girdiğiniz sayı nötr")
+else sayi > 0:
+    print("Pozitif bir sayı girdiniz")
